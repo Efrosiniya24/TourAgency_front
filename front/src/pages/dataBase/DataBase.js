7598946146358764e6f55fa62889e1a527d28f90
frosya <efrosiniya24@gmail.com>
2024-05-14 15:00:40 +0300

    search connection, user info

--- front/src/pages/dataBase/DataBase.js
+++ front/src/pages/dataBase/DataBase.js
@@ -1,8 +1,8 @@
-
 import { NavLink } from "react-router-dom";
 import HeaderAdmin from '../../components/headerAdmin/headerAdmin';
-import dataBase from './DataBase.module.css';
+import SelectedUser from "../../components/headerAdmin/selectedUser/selectedUser";
 
+import dataBase from './DataBase.module.css';
 import databaseIcon from "./photo/database.png";
 import requestIcon from "./photo/application.png";
 import analyticsIcon from "./photo/analytics.png";
@@ -10,23 +10,26 @@ import reportIcon from "./photo/report.png";
 import tourIcon from "./photo/travel.png";
 import up from "./photo/up.png";
 import down from "./photo/down.png";
-
+import searchIcon from "./photo/search2.png";
+import reloadIcon from "./photo/reload.png";
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; 
-
+import axios from 'axios';
 
 const DataBase = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [sortDirection, setSortDirection] = useState('desc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       setError(null);
       try {
-        const response = await axios.get('http://localhost:8000/user/allUsers'); // Замените на URL вашего эндпоинта
+        const response = await axios.get('http://localhost:8000/user/allUsers');
         setUsers(response.data);
       } catch (error) {
         setError(error);
@@ -34,15 +37,47 @@ const DataBase = () => {
         setIsLoading(false);
       }
     };
-
     fetchData();
   }, []);
- 
+
   const handleSortClick = () => {
     setSortDirection(sortDirection === 'desc' ? 'asc' : 'desc');
   };
-  
-  // Сортировка пользователей по ID в зависимости от sortDirection
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await axios.post('http://localhost:8000/user/search', { 
+        name: searchTerm,
+        surname: searchTerm,
+        patronymic: searchTerm,
+        phone: searchTerm,
+        email: searchTerm,
+      });
+      setUsers(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSearchEnter = async (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  const handleReload = () => {
+    window.location.reload();
+  };
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+    setIsVisible(true); 
+  };
+
   const sortedUsers = [...users].sort((a, b) => {
     if (sortDirection === 'asc') {
       return a.id - b.id;
@@ -51,48 +86,44 @@ const DataBase = () => {
     }
   });
 
-  if (isLoading) {
-    return <div>Загрузка...</div>;
-  }
-
   if (error) {
     return <div>Ошибка: {error.message}</div>;
   }
-  
+
   return (
     <div>
-      <HeaderAdmin/>
+      <HeaderAdmin />
       <div className={dataBase.containerDataBase}>
         <aside className={dataBase.sidebar}>
           <nav>
             <ul>
               <li>
                 <NavLink to="#" activeClassName="active">
-                  <img src={databaseIcon}/> 
+                  <img src={databaseIcon} />
                   <p className={dataBase.menuCurrent}>База данных</p>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="#" activeClassName="active">
-                  <img src={requestIcon}/>
+                  <img src={requestIcon} />
                   <p className={dataBase.menu}>Заявки</p>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="#" activeClassName="active">
-                  <img src={analyticsIcon}/>
+                  <img src={analyticsIcon} />
                   <p className={dataBase.menu}>Аналитика</p>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="#" activeClassName="active">
-                  <img src={reportIcon}/>
+                  <img src={reportIcon} />
                   <p className={dataBase.menu}>Отчет</p>
                 </NavLink>
               </li>
               <li>
                 <NavLink to="#" activeClassName="active">
-                  <img src={tourIcon}/>
+                  <img src={tourIcon} />
                   <p className={dataBase.menu}>Туры</p>
                 </NavLink>
               </li>
@@ -100,25 +131,45 @@ const DataBase = () => {
           </nav>
         </aside>
         <main className={dataBase.content}>
-          <div className={dataBase.search}>
-            <input type="text" placeholder="Введите ФИО клиента"/>
+          <div className={dataBase.searchAndUserInfo}>
+            <div className={dataBase.search}>
+              <img 
+                src={searchIcon} 
+                alt="Search" 
+                onClick={handleSearch}
+              />
+              <input 
+                type="text" 
+                placeholder="Введите ФИО клиента"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={handleSearchEnter}
+              />
+              <img 
+                className={dataBase.reloadIcon} 
+                src={reloadIcon} 
+                alt="Reload" 
+                onClick={handleReload} 
+              />
+            </div>
+            {isVisible && <SelectedUser user={selectedUser} dataBase={dataBase} setIsVisible={setIsVisible} />}
           </div>
           <section className={dataBase.data}>
             <table className={dataBase.tableDataBase}>
               <thead>
-              <tr>
-              <th className={dataBase.th_content}>
-              <div>ID</div>
-                <button onClick={handleSortClick} className={dataBase.sort_button}>
-                  <img src={sortDirection === 'asc' ? up : down} alt="Sort direction" className={dataBase.sort_icon} /> 
-                </button>
-              </th>
-              <th className={dataBase.th_content}><div>Клиент</div></th>
-              </tr>
+                <tr>
+                  <th className={dataBase.th_content}>
+                    <div>ID</div>
+                    <button onClick={handleSortClick} className={dataBase.sort_button}>
+                      <img src={sortDirection === 'asc' ? up : down} alt="Sort direction" className={dataBase.sort_icon} /> 
+                    </button>
+                  </th>
+                  <th className={dataBase.th_content}><div>Клиент</div></th>
+                </tr>
               </thead>
               <tbody>
                 {sortedUsers.map((user) => (
-                  <tr key={user.id}>
+                  <tr key={user.id} onClick={() => handleUserClick(user)}>
                     <td>{user.id}</td>
                     <td>{user.surname} {user.name} {user.patronymic}</td>
                   </tr>
@@ -131,5 +182,5 @@ const DataBase = () => {
     </div>
   );
 }
- 
+
 export default DataBase;
